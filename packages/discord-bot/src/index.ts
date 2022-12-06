0050d706d8b5eae42be26a721a6ac0db2cac8fb5
JacobLinCool <jacoblincool@gmail.com>
2022-12-06 13:50:36 +0800

    feat(bot): listen to all events

--- packages/discord-bot/src/index.ts
+++ packages/discord-bot/src/index.ts
@@ -1,9 +1,8 @@
 import { Bot } from "pure-cat";
 import { FileStore } from "pure-cat-store-file";
-import { Events } from "discord.js";
 import { AgentModule } from "./module";
 
-new Bot({ events: [Events.InteractionCreate, Events.MessageCreate] })
+new Bot()
     .use(new FileStore())
     .use(new AgentModule())
     .start()
