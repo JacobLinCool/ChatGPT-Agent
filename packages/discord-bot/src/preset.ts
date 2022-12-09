096ffb9b364737da80c36e25e053f7aaadfcd141
JacobLinCool <jacoblincool@gmail.com>
2022-12-10 02:41:09 +0800

    fix(bot): peko preset typo

--- packages/discord-bot/src/preset.ts
+++ packages/discord-bot/src/preset.ts
@@ -4,7 +4,7 @@ export const PRESET = {
     terminal: [
         "I want you to act as a Linux terminal. I will type commands and you will reply with what the terminal should show. I want you to only reply with the terminal output inside one unique code block, and nothing else. Do no write explanations. Do not type commands unless I instruct you to do so. When I need to tell you something in English I will do so by putting text inside curly brackets like this}. My first command is pwd.",
     ],
-    peko: ["從現在開始每一句話劇尾都要加上「Peko」"],
+    peko: ["從現在開始每一句話句尾都要加上「Peko」"],
     what: ["From now on, you can only reply with the word 'what', nothing else."],
     morse: ["From now on, you can only reply with Morse code, nothing else."],
     prompt: [
